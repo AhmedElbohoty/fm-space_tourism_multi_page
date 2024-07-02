5161ccc4d33de8dce1d4ec551abff16dde074d9f
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2024-07-03 00:08:56 +0300

    Implement fonts

--- src/pages/AppPage.tsx
+++ src/pages/AppPage.tsx
@@ -1,5 +1,41 @@
 function AppPage() {
-  return <div>AppPage</div>;
+  return (
+    <div>
+      <p className="heading-xl">Heading XL</p>
+
+      <br />
+
+      <p className="heading-l">Heading L</p>
+
+      <br />
+
+      <p className="heading-m">Heading M</p>
+
+      <br />
+
+      <p className="heading-s">Heading S</p>
+
+      <br />
+
+      <p className="heading-xs">Heading XS</p>
+
+      <br />
+
+      <p className="subheading-l">Subheading L</p>
+
+      <br />
+
+      <p className="subheading-s">Subheading S</p>
+
+      <br />
+
+      <p className="nav-text">Nav text</p>
+
+      <br />
+
+      <p className="nav-text-bold">Nav text bold</p>
+    </div>
+  );
 }
 
 export default AppPage;
---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,7 @@
+import "./style.css";
+
+function HomePage() {
+  return <div>HomePage</div>;
+}
+
+export default HomePage;
